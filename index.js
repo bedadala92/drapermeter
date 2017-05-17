d0a050f86630e80f0a8c141d5ea516a0df1487ed
Sandeep Bedadala <sandeepbedadala@gmail.com>
2017-05-17 14:27:03 -0400

    added a poll

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var server = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var POLLS_COLLECTION = "polls";
+
+var mongoURI = process.env.MONGOURI || require('./config/db.js').mongoURI;
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost/drapermeter');
+mongoose.connect(mongoURI);
 mongoose.Promise = global.Promise;
+
+//initialize cors
+server.use(cors());
 
 //initialize body-parser
 server.use(bodyParser.json());
@@ -14,12 +22,19 @@ server.use(bodyParser.json());
 // initialize routes
 server.use('/api',require('./routes/api'));
 
+//Connect to the database before starting the application server
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err,database){
+  if(err){
+    console.log(err);
+    process.exit(1);
+  }
+});
+
 //config files
 var db = require('./config/db');
 
 //set our port
 var port = process.env.PORT || 8080;
-
 
 server.use(express.static(__dirname+'/public'));
 
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Poll = require('../models/polls.js');
 
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
 //Get the list of all the polls from database
 router.get('/polls', function(req,res){
   res.send({type: 'GET'});
@@ -9,12 +15,25 @@ router.get('/polls', function(req,res){
 
 // create a new poll to the database
 router.post('/polls', function(req,res){
+  var poll = req.body;
+  poll.createDate = new Date();
+  if(!(req.body.firstName || req.body.lastName)){
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+  db.collection(POLLS_COLLECTION).insertOne(newContact, function(err,doc){
+    if(err){
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
   // var poll = new Poll(req.body);
   // poll.save();
-  Poll.create(req.body).then(function(poll){
-    res.send(poll);
+  // Poll.create(req.body).then(function(poll){
+  //   res.send(poll);
   }); //This does the task of the above two lines
-});
+// });
 
 //update an existing poll in the database
 router.put('/polls/:id', function(req,res){
